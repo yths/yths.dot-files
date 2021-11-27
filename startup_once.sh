079808f6e549335f59bc0ab6293df2f1390297a9
Yannik Schelske <home@yths.de>
2021-11-27 10:43:44 +0100

    feat: add keyboard layout switch

--- startup_once.sh
+++ startup_once.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 ~/repositories/yths.dot-files/qtile/qtile-server/qtile-server &
-nitrogen --restore &
+# nitrogen --restore &
+feh --bg-fill ~/Pictures/Wallpapers/wallpaper.jpg &
 timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)" &
