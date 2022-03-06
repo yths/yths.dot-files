aee7d7a459a046a0a32ac95701574f1c27dca40e
Yannik Schelske <home@yths.de>
2022-03-06 08:17:03 +0100

    feat: add background handling

--- startup_once.sh
+++ startup_once.sh
@@ -2,5 +2,5 @@
 
 ~/repositories/yths.dot-files/qtile/qtile-server/qtile-server &
 # nitrogen --restore &
-feh --bg-fill ~/Pictures/Wallpapers/wallpaper.jpg &
+feh --bg-fill ~/Pictures/Wallpapers/wallpaper_zen-garden.jpg &
 timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)" &
