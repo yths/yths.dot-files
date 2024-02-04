51ca1cb3127fb3b72b14441ea5e09ddb2c8bf853
Yannik Schelske <home@yths.de>
2024-02-04 18:34:38 +0100

    chore: reset repository

--- qtile/qtile-server/go.mod
+++
@@ -1,5 +0,0 @@
-module qtile-server
-
-go 1.17
-
-require github.com/graphql-go/graphql v0.8.0
--- startup_once.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-~/repositories/yths.dot-files/qtile/qtile-server/qtile-server &
-# nitrogen --restore &
-feh --bg-fill ~/Pictures/Wallpapers/wallpaper_zen-garden.jpg &
-timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)" &
