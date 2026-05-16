0803b9021478b7579d38bf6cbba15ddfafad8f29
Yannik Schelske <home@yths.de>
2026-05-16 08:26:00 +0200

    refactor: migrate service state qtile widget to BackgroundPoll

--- configuration/qtile/widgets/service_state.py
+++ configuration/qtile/widgets/service_state.py
@@ -4,31 +4,26 @@ import libqtile.log_utils
 import libqtile.widget.base
 
 
-class WidgetServiceState(libqtile.widget.base.InLoopPollText):
+class WidgetServiceState(libqtile.widget.base.BackgroundPoll):
     def __init__(self, service, warning_color="#ff0000", **config):
-        libqtile.widget.base.InLoopPollText.__init__(self, **config)
+        libqtile.widget.base.BackgroundPoll.__init__(self, "", **config)
         self.service = service
         self.warning_color = warning_color
 
         self.tick_visible = False
 
     def poll(self):
-        p = subprocess.Popen(
-            f"systemctl --user is-active --quiet {self.service}",
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+        result = subprocess.run(
+            ["systemctl", "--user", "is-active", "--quiet", self.service],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=False,
         )
-        stdout, stderr = p.communicate()
 
-        output = ""
-        if p.returncode == 0:
-            if self.tick_visible:
-                output = "·"
-            else:
-                output = " "
+        if result.returncode == 0:
+            output = "·" if self.tick_visible else " "
             self.tick_visible = not self.tick_visible
         else:
             output = f"<span color='{self.warning_color}'>󰒲</span>"
 
-        return f"{output}"
+        return output
